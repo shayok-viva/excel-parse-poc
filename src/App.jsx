faa2295cd5b5d7a07c0c5af5105ca49f45fb37f7
shayok-viva <shayok.ullah@vivasoftdltd.com>
2025-07-03 16:09:29 +0600

    resolving build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import ExcelJsParser from "./excel-js-poc";
-import SheetJsParser from "./sheet-js-poc";
 import ExcelUploader from "./sheet-js-poc/ParseSheetV2";
 
 function App() {
@@ -11,8 +7,6 @@ function App() {
 
   return (
     <>
-      {/* <ExcelJsParser/> */}
-      {/* <SheetJsParser/> */}
       <ExcelUploader />
     </>
   );
