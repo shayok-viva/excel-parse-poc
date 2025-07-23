91a242e5e0adea7bb1e781db5dd5b5ca7cdd69b0
shayok-viva <shayok.ullah@vivasoftdltd.com>
2025-07-23 12:50:10 +0600

    styling changed

--- src/sheet-js-poc/BookingFormWithControl.jsx
+++ src/sheet-js-poc/BookingFormWithControl.jsx
@@ -271,7 +271,7 @@ export default function BookingFormWithControl() {
                             return next;
                           });
                         }}
-                        style={{ width: 60,background:"transparent", border:'0.5px solid gray' }}
+                        style={{ width: 60,background:"transparent", border:'0.5px solid gray', color:'black' }}
                       />
                     ) : (
                       row[c.accessor]
